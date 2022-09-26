a4c2d3848eb5a33f1b2861d802af11f8423425c6
Allen-Vargas <allenbfox@gmial.com>
2022-09-26 16:01:10 -0400

    Navigation Tabs and Stack initial implemented

---
+++ navigation/Stacks/InstitutionsStack.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Institutions from "../../screens/Institutions/Institutions";
+import Detail from "../../screens/Institutions/Detail";
+
+const StackInstitution = createStackNavigator();
+
+const InstitutionsStackScreen = () => {
+  return (
+    <StackInstitution.Navigator>
+      <StackInstitution.Screen
+        name="Institutions Screen"
+        component={Institutions}
+        options={({ navigation }) => ({
+          title: "Instituciones",
+          headerStyle: {
+            backgroundColor: "#f4511e",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+          headerRight: () => (
+            <MaterialCommunityIcons
+              name="plus"
+              size={24}
+              color="white"
+              style={{ marginRight: 10 }}
+              onPress={() => navigation.navigate("Institution Details Screen")}
+            />
+          ),
+        })}
+      />
+      <StackInstitution.Screen
+        name="Institution Details Screen"
+        component={Detail}
+        options={{ title: "Detail" }}
+      />
+    </StackInstitution.Navigator>
+  );
+};
+
+export default InstitutionsStackScreen;
